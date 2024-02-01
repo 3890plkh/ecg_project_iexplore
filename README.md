473ff3ba5ecd495db8f5c74f70486366185b6550
3890plkh <ninbox284@gmail.com>
2024-02-01 13:20:17 +0000

    Started doing basic repo formatting

---
+++ README.md
@@ -0,0 +1,10 @@
+## Prerequisites
+
+Need to download the wdfb Python package. To install run the command: 
+pip install wfdb
+
+Documentation is availble here: https://wfdb.readthedocs.io/en/latest/
+
+Will also need the standard Python data processing libraries (numpy, pandas, scipy, matplotlib).
+
+
---
+++ downloadall.py
@@ -0,0 +1,27 @@
+# This is a script that downloads all data from the MIT-BIH Arrhythmia Database and converts it to a CSV format 
+
+import wfdb as wfdb
+import pandas as pd
+import numpy as np
+
+# Go through all patient records
+for patient in [100,101,102,103,104,105,106,107,108,109,111,112,113,114,115,116,117,118,119,121,122,123,124,200,201,202,203,205,207,208,209,210,212,213,214,215,217,219,220,221,222,223,228,230,231,232,233,234]:
+    #read both the signal (which is a 2D array) and the fields to tell us the info about the data and patient
+    sig,fields= wfdb.rdsamp(str(patient), pn_dir="mitdb")
+    #create empty dict
+    data = {}
+    #convert sample number to time assuming equal time intervals
+    data["time"]=np.linspace(start=0,stop=fields["sig_len"]/fields["fs"],num=fields["sig_len"],endpoint=False)
+    #for every channel
+    for i in range(len(fields["sig_name"])):
+        #create a column for that channel
+        data["{channel}".format(channel=fields["sig_name"][i])]=sig[:,i]
+    #create data frame of the signals
+    data=pd.DataFrame.from_dict(data)
+    #coverts dataframe to csv (change path if required)
+    data.to_csv(path_or_buf=r"Patient data\{patient}.csv".format(patient=patient),sep=",",index=False)
+
+
+
+        
+
