8dff0627cbfefa8d778ceb8c652a0e9d6746ccd4
3890plkh <ninbox284@gmail.com>
2024-02-14 13:55:09 +0000

    Update snippets.py
    Now we cut long >100 seconds snippets into smaller snippets of about 30 seconds, just so we aren't feeding 30 minute chunks into ML model. Also filtered out some of the bad signals (Ones labelled "MISSB" and "TS", these aren't conditions, these are where the ECG machine has gone wrong). Also, if you run the script multiple times, we don't get duplicate files anymore.

--- snippets.py
+++ snippets.py
@@ -1,32 +1,37 @@
 #The intention of this script is to create small snippets of ecg pulses 
 #that have been described as being characteristic of a certain condition in the database
+#If you already have Chunks then this script deletes that creates chunks again (~10minutes)
 
 #please refer to the following resources for understanding the output of this script:
 # Code for dealing with annotations: https://wfdb.readthedocs.io/en/latest/io.html#wfdb-annotations
-# What each annotation means (please take note of this): https://archive.physionet.org/physiobank/annotations.shtml
+# What each annotation means (please take note of this):https://archive.physionet.org/physiobank/database/html/mitdbdir/intro.htm#annotations
 # Online way to view annotations (need to select mitdb database and "show annotations as text"): https://archive.physionet.org/cgi-bin/atm/ATM
 
 import wfdb as wfdb
 import os
 import numpy as np
 import pandas as pd
-from collections import Counter
+import shutil
+
+#delete old version of chunks
+if os.path.isdir(r"Chunks")==True:
+    shutil.rmtree("Chunks")
+
+#create new version
+os.mkdir("Chunks")
 
 #takes an index of start and end of heart rhythm then converts to csv 
 def convertToSnippet(samplefrom,sampleto,sig,fields,condition,patient):
     #create empty dict
     data = {}
     #convert sample number to time assuming equal time intervals
-    data["time"]=np.linspace(start=0,stop=(sampleto-samplefrom)/fields["fs"],num=sampleto-samplefrom)
+    data["time"]=np.linspace(start=0,stop=(sampleto-1-samplefrom)/fields["fs"],num=sampleto-samplefrom)
     #for every channel
     for i in range(len(fields["sig_name"])):
         #create a column for that channel
         data["{channel}".format(channel=fields["sig_name"][i])]=sig[samplefrom:sampleto,i]
     #create data frame of the signals
     data=pd.DataFrame.from_dict(data)
-    #if folder chunks doesn't exist create one
-    if os.path.isdir(r"Chunks")!=True:
-        os.mkdir("Chunks")
     #if folder for that condition doesn't exist, create one
     if os.path.isdir(r"Chunks\{condition}".format(condition=condition))!=True:
         os.mkdir(r"Chunks\{condition}".format(condition=condition))
@@ -40,21 +45,19 @@ def convertToSnippet(samplefrom,sampleto,sig,fields,condition,patient):
     #coverts dataframe to csv and saves in path specified 
     data.to_csv(path_or_buf=r"Chunks\{condition}\{patient}({count}).csv".format(patient=patient,condition=condition,count=count),sep=",",index=False)
 
-
 #go through each patient file
-for patient in [100,101,102,103,104,105,106,107,108,109,111,112,113,114,115,116,117,118,119,121,122,123,124,200,201,202,203,205,207,208,209,210,212,213,214,215,217,219,220,221,222,223,228,230,231,232,233,234]:
+patients=[100,101,102,103,104,105,106,107,108,109,111,112,113,114,115,116,117,118,119,121,122,123,124,200,201,202,203,205,207,208,209,210,212,213,214,215,217,219,220,221,222,223,228,230,231,232,233,234]
+for patient in patients:
+    print("Reading data for patient {patientNumber} ({patientsRead} of {total})".format(patientNumber=patient,patientsRead=patients.index(patient)+1, total=len(patients)))
     #find the sample and annotation file
     sig,fields= wfdb.rdsamp(str(patient), pn_dir="mitdb")
     ann=wfdb.rdann(str(patient) , extension="atr", pn_dir="mitdb" , return_label_elements=["symbol"])
-        
-    #this is a test to see if we create the correct number of snippets of each condition for each patient
-    print(Counter(ann.aux_note).keys())
-    print(Counter(ann.aux_note).values())
         
     #search through the aux notes to find when a new heart beat rhythm has started
     #index where heartbeat rhythm starts and ends
     startIndex=0
     endIndex=0
+    #loop through all annotations for that patient
     for i in range(len(ann.aux_note)):
         #if the heart beat rhythm is deemed to have changed, then aux_note isn't an empty string
         if ann.aux_note[i]!="":
@@ -69,9 +72,30 @@ for patient in [100,101,102,103,104,105,106,107,108,109,111,112,113,114,115,116,
                 startsample=ann.sample[startIndex]
                 endsample=ann.sample[endIndex]
                 #code for the type of heart beat
-                condition=(ann.aux_note[startIndex].replace("(","")).replace("\x00","")
-                #create snippet
-                convertToSnippet(startsample,endsample,sig,fields,condition,patient)
+                code=ann.aux_note[startIndex]
+                condition=(code.replace("(","")).replace("\x00","")
+                #apparently MISSB and TS are indicative of bad data, so ignore them
+                if condition in ["MISSB", "TS"]:
+                    startIndex=i
+                    continue
+
+                #if we don't have 2 seconds of data bother don't bother saving
+                timelengthOfSample=(endsample-startsample+1)/fields["fs"]
+                if timelengthOfSample<2:
+                    startIndex=i
+                    continue
+                #if snippet is too long, chop it up
+                if timelengthOfSample>100:
+                    #want to create snippets of about 30 seconds
+                    numberOfSnippets=timelengthOfSample//30
+                    #these are the sample numbers we use
+                    samplenumbers=np.int_(np.linspace(start=startsample,stop=endsample,num=int(numberOfSnippets)+2,endpoint=True))
+                    #for each section, create a snippet
+                    for j in range(len(samplenumbers)-1):
+                        convertToSnippet(samplenumbers[j],samplenumbers[j+1],sig,fields,condition,patient)
+                #or just create a sample if sample isn't long enough
+                else:
+                    convertToSnippet(startsample,endsample,sig,fields,condition,patient)
                 #then set start index to be i where different aux_note has been read
                 startIndex=i
         #if we reach the end of the data, set endIndex to be i 
@@ -82,8 +106,27 @@ for patient in [100,101,102,103,104,105,106,107,108,109,111,112,113,114,115,116,
             endsample=ann.sample[endIndex]
             #code for the type of heart beat
             condition=(ann.aux_note[startIndex].replace("(","")).replace("\x00","")
-            #create snippet
-            convertToSnippet(startsample,endsample,sig,fields,condition,patient)
+            #apparently MISSB and TS are indicative of bad data, so ignore them
+            if condition in ["MISSB", "TS"]:
+                continue
+
+            #if we dont have 2 seconds of data bother don't bother saving
+            timelengthOfSample=(endsample-startsample)/fields["fs"]
+            if timelengthOfSample<2:
+                continue
+            #if snippet is too long, chop it up
+            if timelengthOfSample>100:
+                #want to create snippets of about 30 seconds
+                numberOfSnippets=timelengthOfSample//30
+                #these are the sample numbers we use
+                samplenumbers=np.int_(np.linspace(start=startsample,stop=endsample,num=int(numberOfSnippets)+2,endpoint=True))
+                #for each section, create a snippet
+                for j in range(len(samplenumbers)-1):
+                    convertToSnippet(samplenumbers[j],samplenumbers[j+1],sig,fields,condition,patient)
+            #or just create a sample if sample isn't long enough
+            else:    
+                #create snippet
+                convertToSnippet(startsample,endsample,sig,fields,condition,patient)
 
 #thanks to a slight formatting anomaly where first sample is bad so wasn't labeled
 #record 106 creates a ghost file in the chunks folder with 0 entries, so need to delete this
