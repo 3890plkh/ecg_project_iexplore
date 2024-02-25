1e85a6b14efd06aba7cafe55293e33731337ed9a
3890plkh <ninbox284@gmail.com>
2024-02-25 22:57:25 +0000

    Create trainingDataSelector.py
    This code selects a percentage of the data to be used to train ML model and
    retains the rest for testing. Currently we are Fourier transforming the data, can just keep the data in its normal form (will probably be the next thing I try).

---
+++ trainingDataSelector.py
@@ -0,0 +1,53 @@
+#Please run snippets.py first to create chunks
+#This code selects a percentage of the data to be used to train ML model and retains the rest for testing. 
+#Currently we are Fourier transforming the data, can just keep the data in its normal form 
+# (will probably be the next thing I try).
+
+from scipy.fft import rfft
+import pandas as pd
+import os
+import numpy as np
+
+#We have an issue where where snippets can have different lengths
+#if we feed in our snippets as FFTs then this isn't an issue? - maybe something to try
+
+#out of all of the snippets we have created, this function selects percentage% of the files from each folder
+#to be used as training data
+#signals are converted into FFTs
+#returns 4 lists: training data signals, training data conditions, then test data signals and test data conditions
+def selectTrainingData(percentage):
+    #if no chunks of ECG data created from "snippets.py", then run snippets
+    if os.path.isdir("Chunks")!=True:
+        import snippets
+
+    #print proportions of data used as training or test data
+    print("Training Data: {trainingpercent}% \nTest Data: {testpercent}%".format(trainingpercent=percentage,testpercent=100-percentage))
+    if percentage>100 or percentage<=0:
+        raise ValueError("Cannot select {percentage}% of data to train model".format(percentage=str(percentage)))
+
+    trainingFiles_snippets=[]
+    trainingFiles_conditions=[]
+    testFiles_snippets=[]
+    testFiles_conditions=[]
+    #for each condition
+    for folderName in os.listdir("Chunks"):
+        #search the folder for that condition
+        folder=os.listdir(r"Chunks\{folderName}".format(folderName=folderName))
+        #calculate number of files to use
+        filesToUse=percentage/100*len(folder)//1+1
+        #loop through all snippets for that condition
+        for i in range(len(folder)):
+            snippetAsDataFrame=pd.read_csv(r"Chunks\{folderName}\{filename}".format(folderName=folderName, filename=folder[i]),usecols=[1])
+            #i+1 is the number of files that will have been selected if we include this index in the training set
+            #if it is greater than the number of files we intend to use, don't add it to the training set
+            if i+1>filesToUse:
+                testFiles_snippets.append(rfft(snippetAsDataFrame[snippetAsDataFrame.columns[0]].to_numpy(dtype=object))[:1000])
+                testFiles_conditions.append(folderName)
+            #else add to training set
+            else:
+                trainingFiles_snippets.append(rfft(snippetAsDataFrame[snippetAsDataFrame.columns[0]].to_numpy(dtype=object))[:1000])
+                trainingFiles_conditions.append(folderName)
+        #Progress indicator - prints message when snippets for a condition have been sorted into training and test sets
+        print("'{folderName}' training data selected".format(folderName=folderName))
+    
+    return trainingFiles_snippets,trainingFiles_conditions,testFiles_snippets,testFiles_conditions
