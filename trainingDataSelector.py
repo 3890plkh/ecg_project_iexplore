f9f8c24b15ebc1f472756ccd311e74cff77fb1b9
3890plkh <ninbox284@gmail.com>
2024-02-26 14:02:46 +0000

    Update trainingDataSelector.py
    Now we can randomise which files for a condition are being used to train the model (previously we just used the first x% of the folder as training data). Also added a second mode for feeding in the raw signals (currently not working very well).

--- trainingDataSelector.py
+++ trainingDataSelector.py
@@ -7,15 +7,18 @@ from scipy.fft import rfft
 import pandas as pd
 import os
 import numpy as np
+import random
 
 #We have an issue where where snippets can have different lengths
 #if we feed in our snippets as FFTs then this isn't an issue? - maybe something to try
 
 #out of all of the snippets we have created, this function selects percentage% of the files from each folder
 #to be used as training data
-#signals are converted into FFTs
+#several modes for different models
+#mode "FT" - signals are converted into FFTs
+#mode "raw" - keep data in raw voltage values
 #returns 4 lists: training data signals, training data conditions, then test data signals and test data conditions
-def selectTrainingData(percentage):
+def selectTrainingData(percentage,mode):
     #if no chunks of ECG data created from "snippets.py", then run snippets
     if os.path.isdir("Chunks")!=True:
         import snippets
@@ -30,9 +33,14 @@ def selectTrainingData(percentage):
     testFiles_snippets=[]
     testFiles_conditions=[]
     #for each condition
-    for folderName in os.listdir("Chunks"):
+    conditions=os.listdir("Chunks")
+    #randomise which order the conditions they are fed into model
+    random.shuffle(conditions)
+    for folderName in conditions:
         #search the folder for that condition
         folder=os.listdir(r"Chunks\{folderName}".format(folderName=folderName))
+        #randomise which data is used in the training dataset
+        random.shuffle(folder)
         #calculate number of files to use
         filesToUse=percentage/100*len(folder)//1+1
         #loop through all snippets for that condition
@@ -41,12 +49,20 @@ def selectTrainingData(percentage):
             #i+1 is the number of files that will have been selected if we include this index in the training set
             #if it is greater than the number of files we intend to use, don't add it to the training set
             if i+1>filesToUse:
-                testFiles_snippets.append(rfft(snippetAsDataFrame[snippetAsDataFrame.columns[0]].to_numpy(dtype=object))[10:510])
-                testFiles_conditions.append(folderName)
+                if mode=="FT":
+                    testFiles_snippets.append(rfft(snippetAsDataFrame[snippetAsDataFrame.columns[0]].to_numpy(dtype=object))[10:510])
+                    testFiles_conditions.append(folderName)
+                elif mode=="raw":
+                    testFiles_snippets.append(snippetAsDataFrame[snippetAsDataFrame.columns[0]].to_numpy(dtype=object)[0:500])
+                    testFiles_conditions.append(folderName)
             #else add to training set
             else:
-                trainingFiles_snippets.append(rfft(snippetAsDataFrame[snippetAsDataFrame.columns[0]].to_numpy(dtype=object))[10:510])
-                trainingFiles_conditions.append(folderName)
+                if mode=="FT":
+                    trainingFiles_snippets.append(rfft(snippetAsDataFrame[snippetAsDataFrame.columns[0]].to_numpy(dtype=object))[10:510])
+                    trainingFiles_conditions.append(folderName)
+                elif mode=="raw":
+                    trainingFiles_snippets.append(snippetAsDataFrame[snippetAsDataFrame.columns[0]].to_numpy(dtype=object)[0:500])
+                    trainingFiles_conditions.append(folderName)
         #Progress indicator - prints message when snippets for a condition have been sorted into training and test sets
         print("'{folderName}' training data selected".format(folderName=folderName))
     
