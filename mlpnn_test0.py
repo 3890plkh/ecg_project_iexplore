adb895ed8f8f1471765431ea98b07ed9b436ef3e
HongyuPeng <hongyustevenpeng@gmail.com>
2024-03-04 05:54:20 +0000

    Add some model types (SVM, SGD, KNN, NC, GP, DT)

--- mlpnn_test0.py
+++ mlpnn_test0.py
@@ -1,8 +1,14 @@
-#Playing with MLPNN from scikit-learn  
-#This will automatically run snippets.py first to create chunks
+# Playing with MLPNN from scikit-learn  
+# This will automatically run snippets.py first to create chunks
 # mainly following this https://www.educative.io/answers/implement-neural-network-for-classification-using-scikit-learn
 
 from sklearn import neural_network as nn
+from sklearn import svm
+from sklearn.linear_model import SGDClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import NearestCentroid
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
 import os
@@ -27,6 +33,14 @@ try:
 except IndexError:
     mode="FT"
 
+#enter a model type in command line
+#if no model type is entered, default to "MLP" as this works
+try:
+    model_type=sys.argv[3]
+except IndexError:
+    model_type="MLP"
+
+
 #creates training data and test data
 trainingFiles_snippets,trainingFiles_conditions,testFiles_snippets,testFiles_conditions=selectTrainingData(percentage,mode)
 
@@ -43,10 +57,11 @@ def plotFT(number):
     plt.show()
 
 #plot some FTs (comment this out if you don't want to see the plots)
+'''
 if mode=="FT":
     for i in random.sample(range(len(trainingFiles_snippets)),10):
         plotFT(int(i))
-
+'''
 #You should not see this print message - if you do then some of the snippets aren't long enough
 #if mode=="FT":
 #    for i in range(len(trainingFiles_snippets)):
@@ -60,8 +75,25 @@ if mode=="FT":
 le = LabelEncoder()
 le.fit(os.listdir("Chunks"))
 
-#default settings for the first MLPNN model - try playing around with it
-model=nn.MLPClassifier(activation="logistic",verbose=True,shuffle=True,learning_rate="adaptive",max_iter=1000)
+#Form model  with its model_type
+if model_type == "MLP":
+    #default settings for the first MLPNN model - try playing around with it
+    model=nn.MLPClassifier(activation="logistic",verbose=False,shuffle=True,learning_rate="adaptive",max_iter=1000)
+    # verbose is turned off for consistency
+elif model_type == "SVM":
+    model=svm.SVC()
+elif model_type == "SGD":
+    model=SGDClassifier()
+elif model_type == "KNN":
+    model=KNeighborsClassifier()
+elif model_type == "NC":  # low score
+    model=NearestCentroid()
+elif model_type == "GP":  # take forever to run
+    model=GaussianProcessClassifier()
+elif model_type == "DT":
+    model=DecisionTreeClassifier()
+else:
+    raise NameError("Unsupported model type")
 
 #train the model (can feed the data in multiple times if you want)
 model.fit(np.abs(trainingFiles_snippets),le.transform(trainingFiles_conditions))
@@ -70,7 +102,7 @@ model.fit(np.abs(trainingFiles_snippets),le.transform(trainingFiles_conditions))
 predictedConditions=model.predict(np.abs(testFiles_snippets))
 #calculate accuracy
 accuracy=accuracy_score(y_true=le.transform(testFiles_conditions),y_pred=predictedConditions)*100
-print("Accuracy of MLPNN Model: " + str(round(accuracy,2)) +"%")
+print("Accuracy of " + model_type + " Model: " + str(round(accuracy,2)) +"%")
 
 #compare to just guessing N every time
 accuracy=accuracy_score(y_true=le.transform(testFiles_conditions),y_pred=le.transform(["N"]*len(testFiles_conditions)))*100
