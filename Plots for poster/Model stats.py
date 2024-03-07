d91fa7b6e2fe50c373d766df02d39a6abdab009c
3890plkh <ninbox284@gmail.com>
2024-03-07 22:13:45 +0000

    Calculating and saving Precision and Recall statistics
    Having done some reading on the different metrics we can use to see how our model is performing (https://scikit-learn.org/stable/modules/model_evaluation.html), it looks like precision and recall could be quite useful as it allows us to see how model is performing for a certain condition. Added functionality to calculate this for each iteration and to do some statistics on our results. Again, everyone run this and we will combine the results on Monday.

--- Plots for poster/Model stats.py
+++ Plots for poster/Model stats.py
@@ -1,7 +1,42 @@
-#Gives us mean and sample standard deviation of accuracies for each model
+#Gives us mean and sample standard deviation of accuracies, precisions and recall for each model
+#Will help us analyse where models are good and bad
 import pandas as pd
+import sys 
 
-data=pd.read_csv("Accuracies80.csv")
-print("After {iterations} iterations".format(iterations=len(data)))
-for model_type in data.columns:    
-    print("{model} - Mean: {mean}%, SD: {sd}%".format(model=model_type,mean=round(data[model_type].mean(axis=0),2),sd=round(data[model_type].std(axis=0),2)))
+try:
+    percentage=sys.argv[1]
+except:
+    percentage=80
+
+#
+accuracydata=pd.read_csv("Accuracies{percentage}.csv".format(percentage=str(percentage)))
+print("After {iterations} iterations:\nMean Accuracy".format(iterations=len(accuracydata)))
+for model_type in accuracydata.columns:
+    #fine to calculate mean accuracy in this way
+    #For the same percentage of training data, same number of test sets will be selected for each iteration
+    #so accuracy for each iteration is (correctly assigned test sets)/(number of test sets per iteration).
+    #So mean of the accuracies is 1/iterations*( sum of ((correctly assigned test sets)/(number of test sets per iteration)))
+    #Since denominators are the same in the sum of ((correctly assigned test sets)/(number of test sets per iteration)),
+    #then the sum is (total correctly assigned test sets)/(number of test sets per iteration)
+    #and mean is (total correctly assigned test sets)/(total number of test sets)
+    #so it is as if we calculated accuracy for alls iterations summed up
+    print("{model} - Mean: {mean}%, SD: {sd}%".format(model=model_type,mean=round(accuracydata[model_type].mean(axis=0),2),sd=round(accuracydata[model_type].std(axis=0),2)))
+
+print("\n")
+
+for model_type in accuracydata.columns:
+    precisiondata=pd.read_csv("Precision Data/Precisions{model}{percentage}.csv".format(model=model_type,percentage=str(percentage)))
+    recalldata=pd.read_csv("Recall Data/Recalls{model}{percentage}.csv".format(model=model_type,percentage=str(percentage)))
+    print("{model} - After {iterations} iterations".format(model=model_type,iterations=len(precisiondata)))
+    #for precision data calculating the mean is a bit questionable, as they might not be equally weighted
+    # since model might not predict same number of that condition each iteration
+    precisionstats=pd.DataFrame({"Mean":precisiondata.mean(),"SD": precisiondata.std()})
+    print("Precision statistics:")
+    print(precisionstats.T.round(2).to_string(index=True))
+    
+    #as per accuracies, calculating mean for recalls is fine as 
+    # there should be same number of files for each condition for the same percentage
+    recallstats=pd.DataFrame({"Mean":recalldata.mean(), "SD": precisiondata.std()})
+    print("Recall statistics:")
+    print(recallstats.T.round(2).to_string(index=True))
+    print("\n")
--- test_all_models.py
+++ test_all_models.py
@@ -9,7 +9,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neighbors import NearestCentroid
 from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn.preprocessing import LabelEncoder
 import os
 import sys
@@ -39,7 +39,14 @@ except:
 models=["MLP","SVM","SGD","KNN","NC","DT"]
 # copied from last comment on: https://stackoverflow.com/questions/34010312/appending-to-one-python-dictionary-key-appends-to-all-for-some-reason
 accuracies= {key:[] for key in models}
+precisionScores= {key:[] for key in models}
+recallScores= {key:[] for key in models}
 times={key:0 for key in models}
+
+if os.path.isdir("Precision Data")==False:
+    os.mkdir("Precision Data")
+if os.path.isdir("Recall Data")==False:
+    os.mkdir("Recall Data")
 
 #turn conditions into numbers - so we can map snippets onto numbers
 #https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.LabelEncoder.html
@@ -69,8 +76,6 @@ for i in range(iterations):
             model=KNeighborsClassifier()
         elif model_type == "NC":  # low score
             model=NearestCentroid()
-    #    elif model_type == "GP":  # take forever to run
-    #        model=GaussianProcessClassifier()
         elif model_type == "DT":
             model=DecisionTreeClassifier()
 
@@ -84,9 +89,12 @@ for i in range(iterations):
         predictedConditions=model.predict(np.abs(testFiles_snippets))
         #calculate accuracy
         accuracy=accuracy_score(y_true=le.transform(testFiles_conditions),y_pred=predictedConditions)*100
-
+        precisionScore=precision_score(y_true=le.transform(testFiles_conditions),y_pred=predictedConditions,average=None,zero_division=np.nan)*100
+        recallScore=recall_score(y_true=le.transform(testFiles_conditions),y_pred=predictedConditions,average=None)*100
         #append accuracy score to the dict
         accuracies[model_type].append(accuracy)
+        precisionScores[model_type].append(precisionScore)
+        recallScores[model_type].append(recallScore)
 
         #progress report
         print("Iteration " + str(i+1) + " - Model " + model_type + " accuracy: " + str(accuracy))
@@ -94,6 +102,16 @@ for i in range(iterations):
 accuracies=pd.DataFrame.from_dict(accuracies)
 #if accuracy.csv exists append new data to end without adding header
 accuracies.to_csv("Accuracies{percentage}.csv".format(percentage=str(percentage)),mode="a",sep=",",header= not os.path.isfile("Accuracies{percentage}.csv".format(percentage=str(percentage))),index=False)
+
+#save precision and recall data
+for model_type in models:
+    #get precisions for every condition for that model
+    precisionforModeltype=pd.DataFrame(precisionScores[model_type],columns=le.inverse_transform(range(len(precisionScores[model_type][0]))))
+    precisionforModeltype.to_csv("Precision Data/Precisions{model}{percentage}.csv".format(model=model_type,percentage=str(percentage)),mode="a",sep=",",header= not os.path.isfile("Precision Data/Precisions{model}{percentage}.csv".format(model=model_type,percentage=str(percentage))),index=False)
+    #get recalls for every condition for that model
+    recallsforModeltype=pd.DataFrame(recallScores[model_type],columns=le.inverse_transform(range(len(recallScores[model_type][0]))))
+    recallsforModeltype.to_csv("Recall Data/Recalls{model}{percentage}.csv".format(model=model_type,percentage=str(percentage)),mode="a",sep=",",header= not os.path.isfile("Recall Data/Recalls{model}{percentage}.csv".format(model=model_type,percentage=str(percentage))),index=False)
+
 #total time taken to train each model
 print("Total time to train {iterations} versions of each model".format(iterations=iterations))
 print(times)
@@ -103,6 +121,12 @@ edges=np.linspace(70,95,101,endpoint=True)
 data=pd.read_csv("Accuracies{percentage}.csv".format(percentage=str(percentage)),sep=",")
 for model in data.columns:
     plt.hist(data[model],bins=edges,label="{model}".format(model=model),alpha=0.6,histtype="bar",ec="black",stacked=False)
+
+total=0
+for folder in os.listdir("Chunks"):
+    total+=len(os.listdir(r"Chunks/{folder}".format(folder=folder)))
+
+percentN=len(os.listdir("Chunks/N"))/total
 plt.legend()
 plt.ylabel("Frequency")
 plt.xlabel("Accuracy (%)")
